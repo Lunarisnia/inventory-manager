079c6f3420160bffbdb5e69042c400d7bd3bde26
AnggaStywn <anggadwisetiyawan3@gmail.com>
2024-12-08 20:47:27 +0700

    bastard

--- internal/borrowlist/borrow_list_controller.go
+++ internal/borrowlist/borrow_list_controller.go
@@ -2,6 +2,7 @@ package borrowlist
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/Lunarisnia/inventory-manager/database/repo"
 	"github.com/gin-gonic/gin"
@@ -20,12 +21,33 @@ func NewBorrowListController(r *gin.RouterGroup, repository *repo.Queries) *Borr
 		repository: repository,
 	}
 	group.GET("/ping", ctl.Ping)
+	group.GET("/getallborrowlist", ctl.GetAllBorrowList)
+	group.POST("/createnewborrowlist", ctl.CreateNewBorrowList)
 
 	return &ctl
 }
 
 func (b *BorrowListController) Ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
+		"foo": "bar",
+	})
+}
+
+func (b *BorrowListController) GetAllBorrowList(c *gin.Context) {
+	resp, _ := b.repository.ListAllBorrowListByUserID(c.Request.Context(), 2)
+	c.JSON(http.StatusOK, gin.H{
+		"foo": resp,
+	})
+}
+func (b *BorrowListController) CreateNewBorrowList(c *gin.Context) {
+	b.repository.CreateBorrowList(c.Request.Context(), repo.CreateBorrowListParams{
+		UserID:    1,
+		ItemID:    1,
+		BorrowAt:  1,
+		CreatedAt: time.Now().Unix(),
+		UpdatedAt: time.Now().Unix(),
+	})
+	c.JSON(200, gin.H{
 		"foo": "bar",
 	})
 }
