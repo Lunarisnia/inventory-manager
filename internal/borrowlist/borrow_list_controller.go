a60350321b2881b466c8d318d5db11a008a62caf
louna <r.arswendo.r@gmail.com>
2024-12-10 01:10:57 +0700

    feat: add list borrow list

--- internal/borrowlist/borrow_list_controller.go
+++ internal/borrowlist/borrow_list_controller.go
@@ -22,6 +22,7 @@ func NewBorrowListController(r *gin.RouterGroup, repository *repo.Queries) *Borr
 	ctl := BorrowListController{
 		repository: repository,
 	}
+	group.GET("", auth.Authorized(), ctl.ListBorrowList)
 	group.POST("/", auth.Authorized(), ctl.BorrowItem)
 	group.GET("/ping", ctl.Ping)
 
@@ -88,5 +89,25 @@ func (b *BorrowListController) BorrowItem(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{
 		"data": newBorrowList,
+	})
+}
+
+func (b *BorrowListController) ListBorrowList(c *gin.Context) {
+	claim, ok := c.Request.Context().Value("jwt").(*auth.JWTClaim)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Internal Server Error",
+		})
+		return
+	}
+	borrowList, err := b.repository.ListActiveBorrowListByUserID(c.Request.Context(), claim.UserID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": borrowList,
 	})
 }
